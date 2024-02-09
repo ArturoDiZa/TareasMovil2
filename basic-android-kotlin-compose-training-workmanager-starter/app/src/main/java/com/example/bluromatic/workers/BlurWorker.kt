0ce3d22e33d5d8f7a833c320122616f346bf4627
NoblesS1x <s20120229@alumnos.itsur.edu.mx>
2024-02-08 22:46:44 -0600

    Avance y termino de la practica

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -4,7 +4,9 @@ import androidx.work.WorkerParameters
 import androidx.work.CoroutineWorker
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
@@ -22,15 +24,31 @@ class BlurWorker (ctx: Context, params : WorkerParameters) : CoroutineWorker(ctx
         return withContext(Dispatchers.IO){
             delay(DELAY_TIME_MILLIS)
             return@withContext try {
-                val picture = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.android_cupcake)
-                val output = blurBitmap(picture,1)
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
+                val resolver = applicationContext.contentResolver
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
+                )
+
+                val output = blurBitmap(picture, blurLevel)
+
+                // Write bitmap to a temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
 
-                makeStatusNotification("Output is $outputUri", applicationContext)
-                Result.success()
-            }catch (throwable: Throwable){
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+                Result.success(outputData)
+            } catch (throwable: Throwable) {
                 Log.e(
-                    TAG,applicationContext.resources.getString(R.string.error_applying_blur),throwable
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
                 )
                 Result.failure()
             }
---
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/CleanupWorker.java
@@ -0,0 +1,2 @@
+package com.example.bluromatic.workers;public class CleanupWorker {
+}
