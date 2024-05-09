f5ceb25412d06a943c0b0e6a99f0d899caf73e46
arturo <s20120174@alumnos.itsur.mx>
2024-05-08 23:01:56 -0600

    incorporacion del server

---
+++ FirebaseServer/ServidorFirebase/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ServidorFirebase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.mensaje = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn.BackColor = System.Drawing.Color.Chartreuse;
+            this.btn.Enabled = false;
+            this.btn.FlatAppearance.BorderSize = 0;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(475, 30);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(94, 67);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Enviar";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mensaje
+            // 
+            this.mensaje.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mensaje.Location = new System.Drawing.Point(10, 30);
+            this.mensaje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mensaje.Multiline = true;
+            this.mensaje.Name = "mensaje";
+            this.mensaje.Size = new System.Drawing.Size(452, 311);
+            this.mensaje.TabIndex = 2;
+            this.mensaje.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Escribe aquí tu mensaje";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(470, 322);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "No hay envios";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.BackColor = System.Drawing.SystemColors.Control;
+            this.btnReset.Enabled = false;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnReset.Location = new System.Drawing.Point(475, 103);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 53);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Limpiar";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(579, 348);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mensaje);
+            this.Controls.Add(this.btn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Servidor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox mensaje;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ FirebaseServer/ServidorFirebase/Form1.cs
@@ -0,0 +1,85 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Google.Apis.Requests.BatchRequest;
+
+namespace ServidorFirebase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //inicializar firebase
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.GetApplicationDefault(),
+                ProjectId = "myfirebase-577b0",
+            });
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // This registration token comes from the client FCM SDKs.
+            var registrationToken = new List<String>() { "cjWXagBeR06-Ba72YdjoET:APA91bGguMUFnJBq5Kqm7I4QanUdINaipOmCxeUEq3BwyJENnMgHYKsiKSOrvZo86sEfwq7Q2W4FAJBjwxnn2u7S1VP9fUrDPpfx9ieJ74fEDSIVbKHVTKE0ISjXZJoxvGS-X1nGypAN" };
+            // Crear mensaje
+            var message = new FirebaseAdmin.Messaging.MulticastMessage()
+            {
+                Tokens = registrationToken,
+                Data = new Dictionary<string, string>()
+                {
+                    { "mensaje", mensaje.Text }
+                },
+                Notification = new Notification() {Body=mensaje.Text},
+                Android = new AndroidConfig()
+                {
+                    Notification = new AndroidNotification()
+                    {
+                        Icon = "stock_ticker_update",
+                        Color = "#ffbb86",
+                    },
+                },
+            };
+            // Send a message to the device corresponding to the provided
+            // registration token.
+            var response = await FirebaseMessaging.DefaultInstance.SendMulticastAsync(message);
+            // Response is a message ID string.
+            if (response.SuccessCount > 0)
+            {
+                label1.Text = "Mensaje enviado";
+                btn.Enabled = false;
+                mensaje.Enabled = false;
+                btnReset.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (mensaje.Text.Length > 0) { 
+                btn.Enabled = true;
+            }
+            else
+            {
+                btn.Enabled = false;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btn.Enabled = true;
+            mensaje.Text= string.Empty;
+            mensaje.Enabled = true;
+            btnReset.Enabled= false;
+            label1.Text = "Sin enviar";
+        }
+    }
+}
